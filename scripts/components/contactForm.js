1060177fb0830c9d89968bccc29af63e955363aa
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-26 12:09:13 +0200

    form validators

--- scripts/components/contactForm.js
+++ scripts/components/contactForm.js
@@ -1,5 +1,25 @@
 /* eslint-disable no-unused-vars */
 
+const validators = {}
+
+validators.name = (value) => {
+    const nameRegex = new RegExp ("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,}$")
+    return nameRegex.test(value.trim())
+}
+
+validators.email = (value) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return emailRegex.test(value.trim())
+}
+
+validators.message = (value) => {
+    return value.trim().length > 6
+}
+
+validators.testAll = (firstname, name, email, message) => {
+    return validators.name(firstname) && validators.name(name) && validators.email(email) && validators.message(message)
+}
+
 export default class formModale {
     modaleNode
     formNod
@@ -24,6 +44,7 @@ export default class formModale {
     submitForm(e){
         e.preventDefault()
         const formdata = new FormData(this.formNod)
+        if(validators.testAll(formdata.get('prenom'), formdata.get('nom'), formdata.get('email'), formdata.get('message')) === false ) return false
         console.log('PRENOM : ',formdata.get('prenom'))
         console.log('NOM : ',formdata.get('nom'))
         console.log('EMAIL : ',formdata.get('email'))
