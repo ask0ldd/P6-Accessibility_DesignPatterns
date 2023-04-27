41640940e7aebabf93dde4083e21c41770fc70c6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-27 09:10:40 +0200

    defined some more custom select methods

--- scripts/components/customSelect.js
+++ scripts/components/customSelect.js
@@ -37,6 +37,55 @@ class CustomSelect extends HTMLElement{
         this.#customSelectLabel.innerHTML = text + `<img class="customSelectArrow" src="./assets/icons/select-arrow.svg"/>`
     }
 
+    // ACCESSIBILITY : keyboard navigation
+    #keyboardListener(e)
+    {
+        if(e.code == "Escape") this.#optionsListOpenClose()
+        if(e.code == "Tab") this.#optionsListOpenClose()
+        if(e.code == "Enter") this.#optionsListOpenClose()
+        if(e.code == "ArrowUp") this.#previousOption()
+        if(e.code == "ArrowDown") this.#nextOption()
+    }
+
+    #previousOption(){
+        const currentHighlightedOption = this.#getHighlightedOption() || this.#getSelectedOption()
+        const currentHighlightedOptionIndex = this.#customSelectOptions.indexOf(currentHighlightedOption)
+        const previousOption = this.#customSelectOptions[currentHighlightedOptionIndex].previousSibling
+        if(previousOption) {
+            //this.#customSelectLabel.setAttribute('aria-activedescendant', previousOption.id)
+            this.#setAsSelected(previousOption)
+            this.#setAsHighlighted(previousOption)
+        }
+    }
+
+    #nextOption(){
+        const currentHighlightedOption = this.#getHighlightedOption() || this.#getSelectedOption()
+        const currentHighlightedOptionIndex = this.#customSelectOptions.indexOf(currentHighlightedOption)
+        const nextOption = this.#customSelectOptions[currentHighlightedOptionIndex].nextSibling
+        if(nextOption) {
+            //this.#customSelectLabel.setAttribute('aria-activedescendant', nextOption.id)
+            this.#setAsSelected(nextOption)
+            this.#setAsHighlighted(nextOption)
+        }
+    }
+
+    #optionsListOpenClose(){
+        const optionsContainer = this.#shadowDOM.querySelector(".customSelectOptionsContainer")
+        const arrow = this.#shadowDOM.querySelector(".customSelectArrow")
+        const isListClosed = optionsContainer.style.display === "none" || optionsContainer.style.display === ""
+        if(isListClosed) { 
+            optionsContainer.style.display = 'flex'
+            arrow.style.transform = "rotate(0deg)"
+            this.#customSelectLabel.setAttribute("aria-expanded", true)
+        }
+        else{ 
+            optionsContainer.style.display = 'none'
+            arrow.style.transform = "rotate(180deg)"
+            this.#customSelectLabel.setAttribute("aria-expanded", false)
+            this.#customSelectLabel.focus()
+        }                  
+    }
+
     // sets an option as selected
     #setAsSelected(customOption){
         this.#customSelectOptions.forEach(option => {
