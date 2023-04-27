4c7771a1985cc9b008db81b1dbe03de5ca4a540a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-27 09:00:50 +0200

    retrieve ghost select options

--- scripts/components/customSelect.js
+++ scripts/components/customSelect.js
@@ -9,7 +9,23 @@ class CustomSelect extends HTMLElement{
         super()
         this.#ghostSelectNode = document.querySelector(ghostSelectSelector)
         this.#shadowDOM = this.attachShadow({ mode: "open" })
+        // retrieve the options of the ghostSelect to build the customSelect view
+        const masterOptions = this.#getMasterSelectOptions()
 
+    }
+
+    #getMasterSelectOptions(){
+        const options = this.#ghostSelectNode.querySelectorAll("option")
+        const formattedOptions = [...options].map(option => {
+            return {
+                value : option.value,
+                label : option.label,
+                selected : option.selected,
+                originalElement : option
+            }
+        })
+
+        return formattedOptions
     }
 
 }
